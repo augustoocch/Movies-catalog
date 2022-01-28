b56180199bb86383e30fa139411362d8db5ab2ae
August.Occh <augusto.occh@gmail.com>
2022-01-28 18:48:18 -0300

    Connection done with the exception class

--- src/mx/com/gm/peliculas/Excepciones/EscrituraDatosex.java
+++ src/mx/com/gm/peliculas/Excepciones/EscrituraDatosex.java
@@ -1,10 +1,11 @@
 package mx.com.gm.peliculas.Excepciones;
-
+import mx.com.gm.peliculas.negocio.CatalogoPeliculasImpl;
 
 public class EscrituraDatosex {
     
     public static String EscrituraDatosex (String nombrePelicula) {
-
+        
+        
         if (nombrePelicula.equals(null)) {
             throw new RuntimeException ("No agrego ningun titulo valido");
         }  
